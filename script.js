3fa032056f054e7ee3f8b3da9834b3c9c07dc8ad
AbdallaMon <abdotlos60@gmail.com>
2024-08-01 09:36:10 +0300

    test

---
+++ script.js
@@ -0,0 +1,21 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const logo = document.querySelector(".animate-logo");
+//   const textElements = document.querySelectorAll(".animate-text");
+//   const buttonElements = document.querySelectorAll(".button");
+
+//   // Animate the logo first
+//   setTimeout(() => {
+//     logo.classList.add("animate");
+//   }, 500);
+
+//   textElements.forEach((el, index) => {
+//     setTimeout(() => {
+//       el.classList.add("animate");
+//     }, (index + 1) * 500);
+//   });
+//   buttonElements.forEach((el, index) => {
+//     setTimeout(() => {
+//       el.classList.add("animate-button");
+//     }, 1500);
+//   });
+// });
